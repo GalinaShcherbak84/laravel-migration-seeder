86ec0153370817ebd73b2dcb307f7aa3e7f00764
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-10 22:28:34 +0200

    stampare a schermo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pacchetto;
 
 class HomeController extends Controller
 {
     public function index(){
+        $pacchetti = Pacchetto::all();
+        dump($pacchetti);
     
-        return view ('home');
+        return view ('home',compact('pacchetti'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
     <div class="container">
-        <h1> I nostri pacchetti di viaggi</h1>
+        <h1> I nostri viaggi</h1>
+        @foreach ($pacchetti as $pacchetto)
+            <div class="card">
+                <h2>{{$pacchetto['destinazione']}}</h2>
+                <p>Durata viaggio {{$pacchetto['giorni']}} giorni</p>
+                <p>Prezzo viaggio {{$pacchetto['prezzo']}},00 euro</p>
+                <p>Vi aspetta: {{$pacchetto['descrizione']}}</p>
+                <p>Data di partenza: {{$pacchetto['partenza']}}</p>
+                <p>Città di partenza: {{$pacchetto['partenza_da']}}</p>
+                <p>Posti disponibili: {{$pacchetto['posti']}}</p>
+            </div>
+        @endforeach
     </div>
 @endsection
